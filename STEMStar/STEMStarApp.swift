692d82cf76a08e78a04ba7e153daa190294318ce
Lily <lsmgoodchild@icloud.com>
2023-07-27 09:02:46 +0100

    Initial Commit

---
+++ STEMStar/STEMStarApp.swift
@@ -0,0 +1,17 @@
+//
+//  STEMStarApp.swift
+//  STEMStar
+//
+//  Created by scholar on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct STEMStarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
